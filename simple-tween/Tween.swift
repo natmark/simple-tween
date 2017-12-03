1523d2fd90c963860cdc667f7773dabcd2728d99
natmark <natmark0918@gmail.com>
2017-12-03 18:33:36 +0900

    :ok: Add animate methods

--- simple-tween/Tween.swift
+++ simple-tween/Tween.swift
@@ -7,7 +7,25 @@
 //
 
 import Foundation
+import RxSwift
 
-class Tween {
+public class Tween {
+    public class func animateTo<T: Numeric>(fromValue: T, toValue: T, duration: TimeInterval, options: UIViewAnimationOptions, animations: @escaping (T) -> Void, completion: ((Bool) -> Void)?) {
 
+        let disposeBag = DisposeBag()
+        let variable = Variable<T>(fromValue)
+
+        variable.asObservable().subscribe({ result in
+            switch result.event {
+            case .next(let element):
+                animations(element)
+            default:
+                break
+            }
+        }).disposed(by: disposeBag)
+
+        UIView.animate(withDuration: duration, delay: 0, options: options, animations: {
+            variable.value = toValue
+        }, completion: completion)
+    }
 }
