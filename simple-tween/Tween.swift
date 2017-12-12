38d41c792d5740f74e08b0f8de3a1bdd3c1f85ca
natmark <natmark0918@gmail.com>
2017-12-12 20:52:53 +0900

    :ok: Implement simple tweening

--- simple-tween/Tween.swift
+++ simple-tween/Tween.swift
@@ -8,6 +8,43 @@
 
 import Foundation
 
-class Tween {
+public class Tween {
+    public var isTweening: Bool = false
 
+    private var timer: Timer?
+    private var endTime: TimeInterval
+    private var time: TimeInterval = 0
+
+    init(_ endTime: TimeInterval) {
+        self.endTime = endTime
+
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+    }
+
+    @objc private func update() {
+        if isTweening {
+            if endTime > time + 0.01 {
+                time += 0.01
+            } else {
+                time = endTime
+                isTweening = false
+                timer?.invalidate()
+            }
+        }
+    }
+
+    public func pause() {
+        timer?.invalidate()
+    }
+
+    public func start() {
+        timer?.fire()
+        if !isTweening {
+            isTweening = true
+        }
+    }
+
+    public func position() -> TimeInterval {
+        return endTime / time
+    }
 }
